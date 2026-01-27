c50bc8a6e2f19468659fa2bb0596e7092cf949da
Alexxx23Daw <agonjim859@g.educaand.es>
2026-01-27 19:07:46 +0100

    proyecto2 hasta darkmode

---
+++ proyecto2/src/components/Header.jsx
@@ -0,0 +1,33 @@
+export const Header = () => {
+
+  const handleClick = (e) => {
+
+    if(e.target.checked){
+      document.documentElement.classList.add('dark');
+    } else{
+      document.documentElement.classList.remove('dark');
+    }
+
+  }
+
+  return (
+    <header className="bg-very-pale-blue dark:bg-red-600 h-[235px] rounded-b-[20px] pt-8 px-6">
+
+        <h1 className="text-very-dark-blue text-2xl font-bold mb-1">Social Media Dashboard</h1>
+        <p className="text-dark-grayish-blue font-bold mb-6">Total Followers: 23,004</p>
+        <hr className="bg-black mb-[19px]" />
+
+        <div className="flex justify-between">
+
+            <p className="text-dark-grayish-blue font-bold">Dark Mode</p>
+            <label htmlFor="darkmode" className="border relative bg-toggle w-12 h-6 rounded-full overflow-hidden cursor-pointer p-[3px]">
+              <input id="darkmode" type="checkbox" className="peer sr-only" onChange={handleClick}/>
+              <div className="w-full h-full  peer-checked:bg-toggle-gradient absolute top-0 left-0"></div>
+              <div className="w-[18px] h-[18px] bg-light-grayish-blue rounded-full peer-checked:translate-x-[24px] transition-all"></div>
+            </label>
+            
+        </div>
+
+    </header>
+  )
+}
